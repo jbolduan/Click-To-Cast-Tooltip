4d92288f6d88da397c1e0af964acc375a54783c7
Jeff Bolduan <jbolduan@umn.edu>
2024-08-10 15:39:46 -0500

    fixed options panel #7

--- Click-To-Cast-Tooltip-Settings.lua
+++ Click-To-Cast-Tooltip-Settings.lua
@@ -1,6 +1,16 @@
 local addonName, addonTable = ...
 
-local f = CreateFrame("Frame")
+local f = CreateFrame("Frame", "ClickToCastTooltipSettingsFrame", InterfaceOptionsFramePanelContainer)
+f.name = "Click-To-Cast Tooltip"
+local category = Settings.RegisterCanvasLayoutCategory(f, f.name)
+f.settingsCategory = category
+Settings.RegisterAddOnCategory(category)
+
+local function OnSettingsChanged(_, setting, value)
+    local variable = setting:GetVariable()
+    addonTable[variable] = value
+    print("Setting changed: " .. setting:GetVariable(), value)
+end
 
 -- These are the default settings for new installs of the addon.
 local defaults = {
@@ -26,10 +36,12 @@ function f:OnEvent(event, addOnName)
         addonTable.db = ClickToCastTooltipDB
 
         -- If any of the default settings are missing, copy them over.
-        if (addonTable.db.buttonColor == nil) then
+        if ((addonTable.db.buttonColor.r == nil) or (addonTable.db.buttonColor.g == nil) or
+            (addonTable.db.buttonColor.b == nil) or (addonTable.db.buttonColor.a == nil)) then
             addonTable.db.buttonColor = CopyTable(defaults.buttonColor)
         end
-        if (addonTable.db.actionColor == nil) then
+        if ((addonTable.db.actionColor.r == nil) or (addonTable.db.actionColor.g == nil) or
+            (addonTable.db.actionColor.b == nil) or (addonTable.db.actionColor.a == nil)) then
             addonTable.db.actionColor = CopyTable(defaults.actionColor)
         end
         if (addonTable.db.showHeader ~= true and addonTable.db.showHeader ~= false) then
@@ -46,11 +58,9 @@ f:SetScript("OnEvent", f.OnEvent)
 
 -- Create the options panel for the addon.
 function f:InitializeOptions()
-    self.panel = CreateFrame("Frame")
-    self.panel.name = "Click-To-Cast-Tooltip"
 
     -- Create a checkbox which will allow the user to toggle the header on the tooltip.
-    local showHeader = CreateFrame("CheckButton", nil, self.panel, "InterfaceOptionsCheckButtonTemplate")
+    local showHeader = CreateFrame("CheckButton", nil, f, "InterfaceOptionsCheckButtonTemplate")
     showHeader:SetPoint("TOPLEFT", 20, -20)
     showHeader.Text:SetText("Show header with divider")
     showHeader:HookScript("OnClick", function(_, btn, down)
@@ -59,7 +69,7 @@ function f:InitializeOptions()
     showHeader:SetChecked(addonTable.db.showHeader)
 
     -- Create a button which will allow the user to change the color of the button text.
-    local buttonColorPicker = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
+    local buttonColorPicker = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
     buttonColorPicker:SetPoint("TopLeft", showHeader, "TopLeft", 20, -30)
     buttonColorPicker:SetSize(100, 25)
     buttonColorPicker:SetText("Button Text Color")
@@ -69,7 +79,7 @@ function f:InitializeOptions()
     end)
 
     -- Create a button which will allow the user to change the color of the action text.
-    local actionColorPicker = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
+    local actionColorPicker = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
     actionColorPicker:SetPoint("TopLeft", buttonColorPicker, "TopLeft", 0, -30)
     actionColorPicker:SetSize(100, 25)
     actionColorPicker:SetText("Action Text Color")
@@ -79,26 +89,42 @@ function f:InitializeOptions()
     end)
 
     -- Create a button which will allow the user to reset the settings to the default values.
-    local resetSettings = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
+    local resetSettings = CreateFrame("Button", nil, f, "UIPanelButtonTemplate")
     resetSettings:SetPoint("TopLeft", actionColorPicker, "TopLeft", 0, -30)
     resetSettings:SetSize(100, 25)
     resetSettings:SetText("Reset Settings")
     resetSettings:SetScript("OnClick", function()
         addonTable.db = CopyTable(defaults)
     end)
-
-    InterfaceOptions_AddCategory(self.panel)
 end
 
 -- Shows the color picker with the callback function
 function ShowColorPicker(r, g, b, a, changedCallback)
-    ColorPickerFrame.hasOpacity, ColorPickerFrame.opacity = (a ~= nil), a
-    ColorPickerFrame.previousValues = {r, g, b, a}
-    ColorPickerFrame.func, ColorPickerFrame.opacityFunc, ColorPickerFrame.cancelFunc = changedCallback, changedCallback,
-        changedCallback
-    ColorPickerFrame:SetColorRGB(r, g, b)
-    ColorPickerFrame:Hide() -- Need to run the OnShow handler.
-    ColorPickerFrame:Show()
+    -- print("r value: " .. r)
+    -- print("g value: " .. g)
+    -- print("b value: " .. b)
+    -- print("a value: " .. a)
+
+    if (ColorPickerFrame.SetupColorPickerAndShow == nil) then
+        ColorPickerFrame.func = changedCallback
+        ColorPickerFrame.hasOpacity = (a ~= nil)
+        ColorPickerFrame.opacityFunc = changedCallback
+        ColorPickerFrame.opacity = a
+        ColorPickerFrame.previousValues = {r, g, b, a}
+        ColorPickerFrame.cancelFunc = changedCallback
+        ColorPickerFrame.extraInfo = changedCallback
+        ColorPickerFrame:SetColorRGB(r, g, b)
+        ColorPickerFrame:Show()
+    else
+        local info = {}
+        info.swatchFunc = changedCallback
+        info.cancelFunc = changedCallback
+        info.hasOpacity = (a ~= nil)
+        -- info.opacity = a
+        info.previousValues = {r, g, b, a}
+        info.r, info.g, info.b, info.opacity = r, g, b, a
+        ColorPickerFrame:SetupColorPickerAndShow(info)
+    end
 end
 
 -- Callback function to protect the color picker from no selection being made
@@ -109,7 +135,7 @@ function ButtonColorCallback(restore)
         newR, newG, newB, newA = unpack(restore)
     else
         -- Something changed
-        newA, newR, newG, newB = OpacitySliderFrame:GetValue(), ColorPickerFrame:GetColorRGB()
+        newA, newR, newG, newB = ColorPickerFrame:GetColorAlpha(), ColorPickerFrame:GetColorRGB()
     end
 
     addonTable.db.buttonColor.r, addonTable.db.buttonColor.g, addonTable.db.buttonColor.b, addonTable.db.buttonColor.a =
@@ -124,7 +150,7 @@ function ActionColorCallback(restore)
         newR, newG, newB, newA = unpack(restore)
     else
         -- Something changed
-        newA, newR, newG, newB = OpacitySliderFrame:GetValue(), ColorPickerFrame:GetColorRGB()
+        newA, newR, newG, newB = ColorPickerFrame:GetColorAlpha(), ColorPickerFrame:GetColorRGB()
     end
 
     addonTable.db.actionColor.r, addonTable.db.actionColor.g, addonTable.db.actionColor.b, addonTable.db.actionColor.a =
@@ -157,6 +183,6 @@ SlashCmdList["CLICKTOCASTTT"] = function(msg)
         print("/ctctt actionColor - Opens the color picker for the action text color")
         print("/ctctt currentActionColor - Prints the current action text color")
     else
-        InterfaceOptionsFrame_OpenToCategory(f.panel)
+        Settings.OpenToCategory(f.settingsCategory.ID)
     end
 end
